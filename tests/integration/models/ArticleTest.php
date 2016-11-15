de26435a1644d3af48f51766b9941a6949669c08
Hoang Nguyen <hoangnh228@gmail.com>
2016-11-15 17:02:38 +0700

    First commit

---
+++ tests/integration/models/ArticleTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Article;
+
+class ArticleTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @test
+     */
+    function it_fetches_trending_articles()
+    {
+        // Given
+        factory(Article::class, 2)->create();
+        factory(Article::class)->create(['reads' => 10]);
+        $mostPopular = factory(Article::class)->create(['reads' => 20]);
+
+        // When
+        $articles = Article::trending();
+
+        // Then
+        $this->assertEquals($mostPopular->id, $articles->first()->id);
+        $this->assertCount(3, $articles);
+    }
+}
