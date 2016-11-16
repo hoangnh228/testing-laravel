75f7289bcfdf0bf56a3e20c0b6725dd86eafc128
Hoang Nguyen <hoangnh228@gmail.com>
2016-11-16 15:23:12 +0700

    Eposite 9 - Liking model with TDD

--- tests/LikesTest.php
+++ tests/LikesTest.php
@@ -6,13 +6,98 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class LikesTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /**
-     * A basic test example.
-     *
-     * @return void
+     * @test
      */
-    public function testExample()
+    public function a_user_can_like_a_post()
     {
-        $this->assertTrue(true);
+        // given I have a post
+        $post = factory(App\Post::class)->create();
+
+        // and a user
+        $user = factory(App\User::class)->create();
+
+        // and that user is logged in
+        $this->actingAs($user);
+
+        // when they like a post
+        $post->like();
+
+        // then we should see avidence in the database, and the post should be liked.
+        $this->seeInDatabase('likes', [
+            'user_id' => $user->id,
+            'likeable_id' => $post->id,
+            'likeable_type' => get_class($post),
+        ]);
+
+        $this->assertTrue($post->isLiked());
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_can_unlike_a_post()
+    {
+        // given I have a post
+        $post = factory(App\Post::class)->create();
+
+        // and a user
+        $user = factory(App\User::class)->create();
+
+        // and that user is logged in
+        $this->actingAs($user);
+
+        // when they like a post
+        $post->like();
+
+        // when they unlike a post
+        $post->unlike();
+
+        // then we should not see avidence in the database, and the post should be unliked.
+        $this->notSeeInDatabase('likes', [
+            'user_id' => $user->id,
+            'likeable_id' => $post->id,
+            'likeable_type' => get_class($post),
+        ]);
+
+        $this->assertFalse($post->isLiked());
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_may_toggle_a_posts_like_status()
+    {
+        $post = factory(App\Post::class)->create();
+
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user);
+
+        $post->toggle();
+
+        $this->assertTrue($post->isLiked());
+
+        $post->toggle();
+
+        $this->assertFalse($post->isLiked());
+    }
+
+    /**
+     * @test
+     */
+    public function a_post_knows_how_many_likes_it_has()
+    {
+        $post = factory(App\Post::class)->create();
+
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user);
+
+        $post->toggle();
+
+        $this->assertEquals(1, $post->likesCount);
     }
 }
